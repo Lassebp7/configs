d385e0bc396393d07402302df13dd729e4f9918a
Lassebp7 <lasseborring@hotmail.com>
2025-07-04 13:23:49 +0200

    merged work config stuff

--- configs/.config/fish/aliases.fish
+++ configs/.config/fish/aliases.fish
@@ -1,8 +1,16 @@
 alias ls 'ls -G --color'
 alias lsa 'ls -a'
+
 alias h 'history | head -n 20'
 alias c 'clear'
+
 alias reload 'source ~/.config/fish/config.fish ; echo Bash config reloaded'
+
+alias now 'date +"%T"'
+
+alias ping 'ping -c 5'
+
 alias .. 'cd ..'
+alias cd.. 'cd ..'
 alias ... 'cd ../..'
 alias .... 'cd ../../..'
--- configs/.config/fish/config.fish
+++ configs/.config/fish/config.fish
@@ -1,6 +1,17 @@
 
 source ~/.config/fish/aliases.fish
 
+set -g fish_user_paths /home/lassebp7/.dotnet/tools
+
+set -gx DOTNET_ROOT (mise where dotnet)
+
+# pnpm
+set -gx PNPM_HOME "/home/lassebp7/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
+
 mise activate fish | source
 starship init fish | source
 
