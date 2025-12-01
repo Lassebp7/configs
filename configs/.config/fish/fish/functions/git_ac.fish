2e2099fbe5da361c7a4902b93e2061e485733092
lassebp7 <lasseborring@hotmail.com>
2025-12-01 11:23:11 +0100

    martini i haanden

--- configs/.config/fish/fish/functions/git_ac.fish
+++
@@ -1,9 +0,0 @@
-function git_ac --description 'Add all changes and commit with a message'
-    if test (count $argv) -eq 0
-        echo "Usage: git ac 'commit message'"
-        return 1
-    end
-
-    git add .
-    git commit -m "$argv"
-end
