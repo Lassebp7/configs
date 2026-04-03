9f209817015aea081d895ed9fb0bc69ff094bf4f
lassebp7 <lasseborring@hotmail.com>
2026-04-03 20:09:00 +0200

    ski bi di?

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,14 @@
+set -gx PATH /usr/local/sbin /usr/local/bin /usr/sbin /usr/bin /sbin /bin
+
+fish_add_path "/mnt/c/Users/lasse/AppData/Local/Programs/Microsoft VS Code/bin"
+fish_add_path "/mnt/c/Program Files/Rancher Desktop/resources/resources/linux/bin"
+fish_add_path "/mnt/c/Program Files/Rancher Desktop/resources/resources/win32/bin"
+fish_add_path "/mnt/c/Windows/System32"
+
+alias code='/mnt/c/Users/lasse/AppData/Local/Programs/Microsoft\ VS\ Code/bin/code'
 
 source ~/.config/fish/aliases.fish
+source ~/.config/fish/abbreviations.fish
 
 mise activate fish | source
 starship init fish | source
